d8b17e9565ee9aa551d062fa09c4127760e41400
Mike Knepper <michaelrknepper@gmail.com>
2014-04-07 16:58:35 -0500

    Refactors personality mock

--- open_closed/good/spec/GreeterTest.java
+++ open_closed/good/spec/GreeterTest.java
@@ -1,32 +1,22 @@
-import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by mrk on 4/7/14.
  */
+class MockPersonality implements Personality {
+    public String greet() {
+        return "foo";
+    }
+}
 
 public class GreeterTest {
-    private Personality mockPersonality;
-
-    @Before
-    public void mockPersonality() {
-        class MockPersonality implements Personality {
-            public String greet() {
-                return "foo";
-            }
-        }
-
-        mockPersonality = new MockPersonality();
-    }
-
     @Test
     public void testGreetsSomeone() {
+        MockPersonality mockPersonality = new MockPersonality();
         Greeter greeter = new Greeter(mockPersonality);
+
         assertEquals("foo", greeter.greet());
     }
 }
